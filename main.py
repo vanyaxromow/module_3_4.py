3b27439bb482c61c4075b487880428aea926cb0d
vanyaxromow <vanya.xromow@yandex.ru>
2024-07-13 11:07:44 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-#
-#
 def single_root_words(root_word, *other_words):
     same_words = []
-    a = str(other_words)
+    # a = list(other_words)
     for i in other_words:
-        if i in root_word or root_word in i:
+        if i.lower() in root_word.lower() or root_word.lower() in i.lower():
             same_words.append(i)
     return same_words
 
@@ -13,13 +11,3 @@ result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
 print(result1)
 print(result2)
-# #
-# #
-# #
-#
-# a = ('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
-# b = str(a)
-# print(type(b), b)
-# c = b.split(sep)
-# print(type(c), c)
-
